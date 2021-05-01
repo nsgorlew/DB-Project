9296617b8c3380521a889eeeecf13c84e45824df
nsgorlew <nsgorlew@gmail.com>
2021-05-01 14:38:33 -0700

    added average fines function

--- admin_functions.py
+++ admin_functions.py
@@ -1,4 +1,5 @@
 import psycopg2
+from decimal import *
 
 def registerAdmin(conn,user,pw):
     try:
@@ -214,3 +215,51 @@ def q5_most_popular_books_by_year(conn,year):
         cur.close()
     except (Exception,psycopg2.DatabaseError) as e:
         print(e)
+        
+def annoying_query(conn,sdate,edate):
+	#BETWEEN sdate and edate
+	try:
+	    cur = conn.cursor()
+	    cur.execute("SELECT BRANCH.BID,BRANCH.LNAME,AVG(RDTIME-BDTIME) AS INTEGER FROM BORROWING,BORROWS,BRANCH WHERE BORROWING.BOR_NO=BORROWS.BOR_NO AND BORROWS.BID=BRANCH.BID AND (RDTIME BETWEEN '%s' AND '%s') AND (BDTIME BETWEEN '%s' AND '%s') GROUP BY BRANCH.BID" % (sdate,edate,sdate,edate))
+	    print("Branch ID | Branch Name | Average")
+	    print("-"*80)
+	    while True:
+	        datalist = []
+	        row = cur.fetchone()
+	        if row == None:
+	            break
+	        for item in row:
+	    	    datalist.append(item)
+	        if datalist[2] > 20:
+	            datalist[2] = (datalist[2]-20)*Decimal(0.2)
+	        else:
+	            datalist[2] = 0
+	        print("Branch ID: %s | Branch Name: %s | Average Fine: %s"%(datalist[0],datalist[1],round(datalist[2],2)))
+	    cur.close()
+	    #cur.execute("select name, id, avg (condition for avg. fine from sdate to edate) from borrows, borrowing, branch where borrows.bor_no = borrowing.bor_no and borrows.bid= branch.bid")
+	    #day_num_list = []
+	    #branch_id_list = []
+	    #branch_name_list = []
+	    #fine = 0
+
+	    #while True:
+	        #row = cur.fetchone()
+	        #if row == None:
+	            #break
+	        #branch_id_list.append(row[0])
+	        #branch_name_list.append(row[1])
+	        #day_num_list.append(int(row[2]))
+	    #index = 0
+	    #for index in len(day_num_list):
+	      #  if day_num_list[day] > 20:
+	        #    fine = (day_num_list[day]-20)*0.20
+	       # elif day_num_list[day] < 20:
+	      #      fine = fine
+	      #  index = index + 1
+	    #average_fine = fine / len(day_num_list)
+	   # for branch in branch_id_list:
+	        #print("Branch ID: %s, Branch Name: %s, Average fine: %s" %(branch_id_list[branch],branch_name_list[branch],average_fine)
+	except (Exception,psycopg2.DatabaseError) as e:
+	    print(e)
+
+
